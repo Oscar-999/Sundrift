08051fd16826d0b67b29fbfb4b949cd30cd9bc8c
oscar <oscarvac74@gmail.com>
2024-02-19 20:08:38 -0600

    react

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import CategoryItem from '../category-item/CategoryItem'
-import './directory.styles.scss'
+import React from 'react';
+import CategoryItem from '../category-item/CategoryItem';
+import './directory.styles.scss';
 
-function Directory({categories}) {
+function Directory({ categories }) {
   return (
     <div className="directory-container">
-    {categories.map(({ category }) => (
-      <CategoryItem category={category} key={category.id} />
-    ))}
-  </div>
-  )
+      {categories.map(category => ( // Changed destructuring to directly access each category object
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
 }
 
-export default Directory
+export default Directory;
