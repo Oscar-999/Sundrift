1cfe5019809567c85eda5a8cb2bfa3636efe8dea
oscar <oscarvac74@gmail.com>
2024-02-20 10:30:45 -0600

    setting up firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation";
 import Home from "./routes/home/home";
-
+import SignIn from "./routes/sign-in/SignIn";
 
 
 
@@ -14,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
