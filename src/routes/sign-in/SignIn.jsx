c185732fdead8e8de95a2c04cb41d4ff1be33a68
oscar <oscarvac74@gmail.com>
2024-02-20 16:44:30 -0600

    sigin in  with redirect

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,16 +1,34 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utill"
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+  auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utill";
 
 const SignIn = () => {
-    const logGoogleUser = async() => {
-        const {user} = await signInWithGooglePopup()
-        const userDocRef = await createUserDocumentFromAuth(user)
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user)
     }
+  }, []);
+
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
 
   return (
     <div>
-        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
