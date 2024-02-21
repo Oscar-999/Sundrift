1616bf23d0bb1407a1461bded3a120abe95bd051
oscar <oscarvac74@gmail.com>
2024-02-20 19:20:53 -0600

    forminput

---
+++ src/components/form-input/FormInput.jsx
@@ -0,0 +1,13 @@
+import "./form-input.styles.scss";
+import React from "react";
+
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...otherProps} />
+    </div>
+  );
+};
+
+export default FormInput;
