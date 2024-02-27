406936a835c067b2cc737ddcb923bfde706da342
oscar <oscarvac74@gmail.com>
2024-02-26 18:47:58 -0600

    selector

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import App from "./App";
+import App from './App';
+import { CategoriesProvider } from './contexts/categories.context';
+import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 
-import { CategoriesProvider } from "./contexts/categories.context";
-import { CartProvider } from "./contexts/cart.context";
+import './index.scss';
 
-import "./index.scss";
-import { store } from "./store/store";
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
